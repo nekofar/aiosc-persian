ef6e34a9e309485e22862fd9cfc637fbc1e1973b
Milad Nekofar <milad@nekofar.com>
2015-11-01 12:40:45 +0330

    added initial commit to the repository

---
+++ di-aiosc-persian.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	Plugin Name: AIO Support Center Persian
+	Plugin URI: http://github.com/nekofar/aiosc-persian
+	Description: Persian improvements for AIO Support Center.
+	Version: 0.1.0
+	Author: Milad Nekofar
+	Author URI: http://milad.nekofar.com
+	Text Domain: aiosc-persian
+	Domain Path: /languages
+*/
+
+// prevent file from loading outside wordpress
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// load plugin text domain after other plugins load
+add_action( 'plugins_loaded', function () {
+	// configure plugin language path
+	load_plugin_textdomain( 'aiosc', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+} );
+
+// add css file to the admin header
+add_action( 'admin_enqueue_scripts', function () {
+	wp_register_style( 'aiosc-persian', plugins_url( '/assets/css/styles.css', __FILE__ ) );
+	wp_enqueue_style( 'aiosc-persian' );
+} );
